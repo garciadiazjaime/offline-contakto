15102a40da06b8fc6dadf34e6aef05738cd376c9
jaime <garciadiazjaime@gmail.com>
2021-02-10 15:27:13 -0600

    feat: [https://trello.com/c/qA2Nnh4j] info econ

--- src/components/candidato/informacion-economica.svelte
+++ src/components/candidato/informacion-economica.svelte
@@ -1,5 +1,58 @@
 <script>
 	export let user
+
+	$: user.info_economica_mensual.ingresos[5].monto = 0 
+		+ parseFloat(user.info_economica_mensual.ingresos[0].monto || 0)
+		+ parseFloat(user.info_economica_mensual.ingresos[1].monto || 0)
+		+ parseFloat(user.info_economica_mensual.ingresos[2].monto || 0)
+		+ parseFloat(user.info_economica_mensual.ingresos[3].monto || 0)
+		+ parseFloat(user.info_economica_mensual.ingresos[4].monto || 0)
+	
+	$: user.info_economica_mensual.egresos[14].monto = 0
+		+ parseFloat(user.info_economica_mensual.egresos[0].monto || 0)
+		+ parseFloat(user.info_economica_mensual.egresos[1].monto || 0)
+		+ parseFloat(user.info_economica_mensual.egresos[2].monto || 0)
+		+ parseFloat(user.info_economica_mensual.egresos[3].monto || 0)
+		+ parseFloat(user.info_economica_mensual.egresos[4].monto || 0)
+		+ parseFloat(user.info_economica_mensual.egresos[5].monto || 0)
+		+ parseFloat(user.info_economica_mensual.egresos[6].monto || 0)
+		+ parseFloat(user.info_economica_mensual.egresos[7].monto || 0)
+		+ parseFloat(user.info_economica_mensual.egresos[8].monto || 0)
+		+ parseFloat(user.info_economica_mensual.egresos[9].monto || 0)
+		+ parseFloat(user.info_economica_mensual.egresos[10].monto || 0)
+		+ parseFloat(user.info_economica_mensual.egresos[11].monto || 0)
+		+ parseFloat(user.info_economica_mensual.egresos[12].monto || 0)
+		+ parseFloat(user.info_economica_mensual.egresos[13].monto || 0)
+		
+		
+
+
+
+	function infonavitChange(event) {
+		if (event.target.value === 'NO') {
+			user.datos_generales.infonavit.numero = 'no aplica'
+			user.datos_generales.infonavit.fecha_tramite = 'no aplica'
+			user.datos_generales.infonavit.uso = 'no aplica'
+			user.datos_generales.infonavit.motivo = 'no aplica'			
+		} else {
+			user.datos_generales.infonavit.numero = ''
+			user.datos_generales.infonavit.fecha_tramite = ''
+			user.datos_generales.infonavit.uso = ''
+			user.datos_generales.infonavit.motivo = ''			
+		}
+	}
+
+	function fonacotChange(event) {
+		if (event.target.value === 'NO') {
+			user.datos_generales.fonacot.numero = 'no aplica'
+			user.datos_generales.fonacot.fecha_tramite = 'no aplica'
+			user.datos_generales.fonacot.uso = 'no aplica'
+		} else {
+			user.datos_generales.fonacot.numero = ''
+			user.datos_generales.fonacot.fecha_tramite = ''
+			user.datos_generales.fonacot.uso = ''
+		}
+	}
 </script>
 
 <style>
@@ -36,7 +89,7 @@
 
 <p>
 	<span>Dependientes economicos</span>
-	<input bind:value={user.datos_generales.dependientes_economicos}>
+	<input bind:value={user.datos_generales.dependientes_economicos} required>
 </p>
 
 <h2>Infonavit</h2>
@@ -50,16 +103,16 @@
 	</tr>
 	<tr>
 		<td>
-			<select bind:value={user.datos_generales.infonavit.activo}>
-				<option value=""></option>
+			<select bind:value={user.datos_generales.infonavit.activo} on:change={infonavitChange}>
+				<option value="">Seleccionar</option>
 				<option value="SI">Sí</option>
 				<option value="NO">No</option>
 			</select>
 		</td>
-		<td><input bind:value={user.datos_generales.infonavit.numero}></td>
-		<td><input bind:value={user.datos_generales.infonavit.fecha_tramite}></td>
-		<td><input bind:value={user.datos_generales.infonavit.uso}></td>
-		<td><input bind:value={user.datos_generales.infonavit.motivo}></td>
+		<td><input bind:value={user.datos_generales.infonavit.numero} required></td>
+		<td><input bind:value={user.datos_generales.infonavit.fecha_tramite} required></td>
+		<td><input bind:value={user.datos_generales.infonavit.uso} required></td>
+		<td><input bind:value={user.datos_generales.infonavit.motivo} required></td>
 	</tr>
 </table>
 
@@ -73,15 +126,15 @@
 	</tr>
 	<tr>
 		<td>
-			<select bind:value={user.datos_generales.fonacot.activo}>
-				<option value=""></option>
+			<select bind:value={user.datos_generales.fonacot.activo} on:change={fonacotChange}>
+				<option value="">Seleccionar</option>
 				<option value="SI">Sí</option>
 				<option value="NO">No</option>
 			</select>
 		</td>
-		<td><input bind:value={user.datos_generales.fonacot.numero}></td>
-		<td><input bind:value={user.datos_generales.fonacot.fecha_tramite}></td>
-		<td><input bind:value={user.datos_generales.fonacot.uso}></td>
+		<td><input bind:value={user.datos_generales.fonacot.numero} required></td>
+		<td><input bind:value={user.datos_generales.fonacot.fecha_tramite} required></td>
+		<td><input bind:value={user.datos_generales.fonacot.uso} required></td>
 	</tr>
 </table>
 
@@ -96,12 +149,12 @@
 		<th>Total</th>
 	</tr>
 	<tr>
-		<td><input bind:value={user.info_economica_mensual.ingresos[0].monto}></td>
-		<td><input bind:value={user.info_economica_mensual.ingresos[1].monto}></td>
-		<td><input bind:value={user.info_economica_mensual.ingresos[2].monto}></td>
-		<td><input bind:value={user.info_economica_mensual.ingresos[3].monto}></td>
-		<td><input bind:value={user.info_economica_mensual.ingresos[4].monto}></td>
-		<td><input bind:value={user.info_economica_mensual.ingresos[5].monto}></td>
+		<td><input bind:value={user.info_economica_mensual.ingresos[0].monto} placeholder="$m.n." required></td>
+		<td><input bind:value={user.info_economica_mensual.ingresos[1].monto} placeholder="$m.n." required></td>
+		<td><input bind:value={user.info_economica_mensual.ingresos[2].monto} placeholder="$m.n." required></td>
+		<td><input bind:value={user.info_economica_mensual.ingresos[3].monto} placeholder="$m.n." required></td>
+		<td><input bind:value={user.info_economica_mensual.ingresos[4].monto} placeholder="$m.n." required></td>
+		<td><input bind:value={user.info_economica_mensual.ingresos[5].monto} placeholder="$m.n." readonly></td>
 	</tr>
 </table>
 
@@ -115,11 +168,11 @@
 		<th>Alimentación</th>
 	</tr>
 	<tr>
-		<td><input bind:value={user.info_economica_mensual.egresos[0].monto}></td>
-		<td><input bind:value={user.info_economica_mensual.egresos[1].monto}></td>
-		<td><input bind:value={user.info_economica_mensual.egresos[2].monto}></td>
-		<td><input bind:value={user.info_economica_mensual.egresos[3].monto}></td>
-		<td><input bind:value={user.info_economica_mensual.egresos[4].monto}></td>
+		<td><input bind:value={user.info_economica_mensual.egresos[0].monto} placeholder="$m.n." required></td>
+		<td><input bind:value={user.info_economica_mensual.egresos[1].monto} placeholder="$m.n." required></td>
+		<td><input bind:value={user.info_economica_mensual.egresos[2].monto} placeholder="$m.n." required></td>
+		<td><input bind:value={user.info_economica_mensual.egresos[3].monto} placeholder="$m.n." required></td>
+		<td><input bind:value={user.info_economica_mensual.egresos[4].monto} placeholder="$m.n." required></td>
 	</tr>
 
 	<tr><td colspan="5"><br /></td></tr>
@@ -132,11 +185,11 @@
 		<th>Serv. doméstico</th>
 	</tr>
 	<tr>
-		<td><input bind:value={user.info_economica_mensual.egresos[5].monto}></td>
-		<td><input bind:value={user.info_economica_mensual.egresos[6].monto}></td>
-		<td><input bind:value={user.info_economica_mensual.egresos[7].monto}></td>
-		<td><input bind:value={user.info_economica_mensual.egresos[8].monto}></td>
-		<td><input bind:value={user.info_economica_mensual.egresos[9].monto}></td>
+		<td><input bind:value={user.info_economica_mensual.egresos[5].monto} placeholder="$m.n." required></td>
+		<td><input bind:value={user.info_economica_mensual.egresos[6].monto} placeholder="$m.n." required></td>
+		<td><input bind:value={user.info_economica_mensual.egresos[7].monto} placeholder="$m.n." required></td>
+		<td><input bind:value={user.info_economica_mensual.egresos[8].monto} placeholder="$m.n." required></td>
+		<td><input bind:value={user.info_economica_mensual.egresos[9].monto} placeholder="$m.n." required></td>
 	</tr>
 
 	<tr><td colspan="5"><br /></td></tr>
@@ -149,11 +202,11 @@
 		<th>Total</th>
 	</tr>
 	<tr>
-		<td><input bind:value={user.info_economica_mensual.egresos[10].monto}></td>
-		<td><input bind:value={user.info_economica_mensual.egresos[11].monto}></td>
-		<td><input bind:value={user.info_economica_mensual.egresos[12].monto}></td>
-		<td><input bind:value={user.info_economica_mensual.egresos[13].monto}></td>
-		<td><input bind:value={user.info_economica_mensual.egresos[14].monto}></td>
+		<td><input bind:value={user.info_economica_mensual.egresos[10].monto} placeholder="$m.n." required></td>
+		<td><input bind:value={user.info_economica_mensual.egresos[11].monto} placeholder="$m.n." required></td>
+		<td><input bind:value={user.info_economica_mensual.egresos[12].monto} placeholder="$m.n." required></td>
+		<td><input bind:value={user.info_economica_mensual.egresos[13].monto} placeholder="$m.n." required></td>
+		<td><input bind:value={user.info_economica_mensual.egresos[14].monto} placeholder="$m.n." readonly></td>
 	</tr>
 </table>
 
@@ -165,18 +218,18 @@
 		<th>Pago Mínimo</th>
 		<th>Saldo Actual</th>
 	</tr>
-	<tr>
-		<td><input bind:value={user.situacion_economica.tarjetas_credito_comerciales[0].institucion}></td>
-		<td><input bind:value={user.situacion_economica.tarjetas_credito_comerciales[0].limite_credito}></td>
-		<td><input bind:value={user.situacion_economica.tarjetas_credito_comerciales[0].pago_minimo}></td>
-		<td><input bind:value={user.situacion_economica.tarjetas_credito_comerciales[0].saldo_actual}></td>
-	</tr>
-	<tr>
-		<td><input bind:value={user.situacion_economica.tarjetas_credito_comerciales[1].institucion}></td>
-		<td><input bind:value={user.situacion_economica.tarjetas_credito_comerciales[1].limite_credito}></td>
-		<td><input bind:value={user.situacion_economica.tarjetas_credito_comerciales[1].pago_minimo}></td>
-		<td><input bind:value={user.situacion_economica.tarjetas_credito_comerciales[1].saldo_actual}></td>
-	</tr>
+	{#each [0, 1] as index}
+		<tr>
+			<td>
+				
+					<input bind:value={user.situacion_economica.tarjetas_credito_comerciales[index].institucion} required={index === 0 ? true : false}>
+				
+			</td>
+			<td><input bind:value={user.situacion_economica.tarjetas_credito_comerciales[index].limite_credito} placeholder="$m.n." required={index === 0 ? true : false}></td>
+			<td><input bind:value={user.situacion_economica.tarjetas_credito_comerciales[index].pago_minimo} placeholder="$m.n." required={index === 0 ? true : false}></td>
+			<td><input bind:value={user.situacion_economica.tarjetas_credito_comerciales[index].saldo_actual} placeholder="$m.n." required={index === 0 ? true : false}></td>
+		</tr>
+	{/each}
 </table>
 
 <h2>Cuentas de débito</h2>
@@ -187,18 +240,14 @@
 		<th>Antiguedad</th>
 		<th>Ahorro</th>
 	</tr>
-	<tr>
-		<td><input bind:value={user.situacion_economica.cuentas_debito[0].institucion}></td>
-		<td><input bind:value={user.situacion_economica.cuentas_debito[0].saldo_mensual}></td>
-		<td><input bind:value={user.situacion_economica.cuentas_debito[0].antiguedad}></td>
-		<td><input bind:value={user.situacion_economica.cuentas_debito[0].ahorro}></td>
-	</tr>
-	<tr>
-		<td><input bind:value={user.situacion_economica.cuentas_debito[1].institucion}></td>
-		<td><input bind:value={user.situacion_economica.cuentas_debito[1].saldo_mensual}></td>
-		<td><input bind:value={user.situacion_economica.cuentas_debito[1].antiguedad}></td>
-		<td><input bind:value={user.situacion_economica.cuentas_debito[1].ahorro}></td>
-	</tr>
+	{#each [0, 1] as index}
+		<tr>
+			<td><input bind:value={user.situacion_economica.cuentas_debito[index].institucion} required={index === 0 ? true : false}></td>
+			<td><input bind:value={user.situacion_economica.cuentas_debito[index].saldo_mensual} placeholder="$m.n." required={index === 0 ? true : false}></td>
+			<td><input bind:value={user.situacion_economica.cuentas_debito[index].antiguedad} required={index === 0 ? true : false}></td>
+			<td><input bind:value={user.situacion_economica.cuentas_debito[index].ahorro} placeholder="$m.n." required={index === 0 ? true : false}></td>
+		</tr>
+	{/each}
 </table>
 
 <h2>Automóviles</h2>
@@ -209,18 +258,14 @@
 		<th>Liquidad</th>
 		<th>Valor Comercial (M.N.)</th>
 	</tr>
-	<tr>
-		<td><input bind:value={user.situacion_economica.automoviles[0].marca}></td>
-		<td><input bind:value={user.situacion_economica.automoviles[0].modelo_ano}></td>
-		<td><input bind:value={user.situacion_economica.automoviles[0].liquidacion}></td>
-		<td><input bind:value={user.situacion_economica.automoviles[0].valor_comercial}></td>
-	</tr>
-	<tr>
-		<td><input bind:value={user.situacion_economica.automoviles[1].marca}></td>
-		<td><input bind:value={user.situacion_economica.automoviles[1].modelo_ano}></td>
-		<td><input bind:value={user.situacion_economica.automoviles[1].liquidacion}></td>
-		<td><input bind:value={user.situacion_economica.automoviles[1].valor_comercial}></td>
-	</tr>
+	{#each [0, 1] as index}
+		<tr>
+			<td><input bind:value={user.situacion_economica.automoviles[index].marca} required={index === 0 ? true : false}></td>
+			<td><input bind:value={user.situacion_economica.automoviles[index].modelo_ano} required={index === 0 ? true : false}></td>
+			<td><input bind:value={user.situacion_economica.automoviles[index].liquidacion} required={index === 0 ? true : false}></td>
+			<td><input bind:value={user.situacion_economica.automoviles[index].valor_comercial} placeholder="$m.n." required={index === 0 ? true : false}></td>
+		</tr>
+	{/each}
 </table>
 
 <h2>Bienes Raíces</h2>
@@ -231,18 +276,14 @@
 		<th>Liquidado</th>
 		<th>Valor Comercial (M.N.)</th>
 	</tr>
-	<tr>
-		<td><input bind:value={user.situacion_economica.bienes_raices[0].tipo_inmueble}></td>
-		<td><input bind:value={user.situacion_economica.bienes_raices[0].ubicacion}></td>
-		<td><input bind:value={user.situacion_economica.bienes_raices[0].liquidacion}></td>
-		<td><input bind:value={user.situacion_economica.bienes_raices[0].valor_comercial}></td>
-	</tr>
-	<tr>
-		<td><input bind:value={user.situacion_economica.bienes_raices[1].tipo_inmueble}></td>
-		<td><input bind:value={user.situacion_economica.bienes_raices[1].ubicacion}></td>
-		<td><input bind:value={user.situacion_economica.bienes_raices[1].liquidacion}></td>
-		<td><input bind:value={user.situacion_economica.bienes_raices[1].valor_comercial}></td>
-	</tr>
+	{#each [0, 1] as index}
+		<tr>
+			<td><input bind:value={user.situacion_economica.bienes_raices[index].tipo_inmueble} required={index === 0 ? true : false}></td>
+			<td><input bind:value={user.situacion_economica.bienes_raices[index].ubicacion} required={index === 0 ? true : false}></td>
+			<td><input bind:value={user.situacion_economica.bienes_raices[index].liquidacion} required={index === 0 ? true : false}></td>
+			<td><input bind:value={user.situacion_economica.bienes_raices[index].valor_comercial} placeholder="$m.n." required={index === 0 ? true : false}></td>
+		</tr>
+	{/each}
 </table>
 
 <h2>Seguros</h2>
@@ -253,18 +294,14 @@
 		<th>Forma de pago</th>
 		<th>Vigencia</th>
 	</tr>
-	<tr>
-		<td><input bind:value={user.situacion_economica.seguros[0].empresa}></td>
-		<td><input bind:value={user.situacion_economica.seguros[0].tipo}></td>
-		<td><input bind:value={user.situacion_economica.seguros[0].forma_pago}></td>
-		<td><input bind:value={user.situacion_economica.seguros[0].vigencia}></td>
-	</tr>
-	<tr>
-		<td><input bind:value={user.situacion_economica.seguros[1].empresa}></td>
-		<td><input bind:value={user.situacion_economica.seguros[1].tipo}></td>
-		<td><input bind:value={user.situacion_economica.seguros[1].forma_pago}></td>
-		<td><input bind:value={user.situacion_economica.seguros[1].vigencia}></td>
-	</tr>
+	{#each [0, 1] as index}
+		<tr>
+			<td><input bind:value={user.situacion_economica.seguros[index].empresa} required={index === 0 ? true : false}></td>
+			<td><input bind:value={user.situacion_economica.seguros[index].tipo} required={index === 0 ? true : false}></td>
+			<td><input bind:value={user.situacion_economica.seguros[index].forma_pago} required={index === 0 ? true : false}></td>
+			<td><input bind:value={user.situacion_economica.seguros[index].vigencia} required={index === 0 ? true : false}></td>
+		</tr>
+	{/each}
 </table>
 
 <h2>Deudas actuales</h2>
@@ -277,20 +314,14 @@
 		<th>Saldo actual</th>
 		<th>Pago mensual</th>
 	</tr>
-	<tr>
-		<td><input bind:value={user.situacion_economica.deudas_actuales[0].fecha_otorgamiento}></td>
-		<td><input bind:value={user.situacion_economica.deudas_actuales[0].tipo}></td>
-		<td><input bind:value={user.situacion_economica.deudas_actuales[0].institucion}></td>
-		<td><input bind:value={user.situacion_economica.deudas_actuales[0].cantidad_total}></td>
-		<td><input bind:value={user.situacion_economica.deudas_actuales[0].saldo_actual}></td>
-		<td><input bind:value={user.situacion_economica.deudas_actuales[0].pago_mensual}></td>
-	</tr>
-	<tr>
-		<td><input bind:value={user.situacion_economica.deudas_actuales[1].fecha_otorgamiento}></td>
-		<td><input bind:value={user.situacion_economica.deudas_actuales[1].tipo}></td>
-		<td><input bind:value={user.situacion_economica.deudas_actuales[1].institucion}></td>
-		<td><input bind:value={user.situacion_economica.deudas_actuales[1].cantidad_total}></td>
-		<td><input bind:value={user.situacion_economica.deudas_actuales[1].saldo_actual}></td>
-		<td><input bind:value={user.situacion_economica.deudas_actuales[1].pago_mensual}></td>
-	</tr>
+	{#each [0, 1] as index}
+		<tr>
+			<td><input bind:value={user.situacion_economica.deudas_actuales[index].fecha_otorgamiento} required={index === 0 ? true : false}></td>
+			<td><input bind:value={user.situacion_economica.deudas_actuales[index].tipo} required={index === 0 ? true : false}></td>
+			<td><input bind:value={user.situacion_economica.deudas_actuales[index].institucion} required={index === 0 ? true : false}></td>
+			<td><input bind:value={user.situacion_economica.deudas_actuales[index].cantidad_total} placeholder="$m.n." required={index === 0 ? true : false}></td>
+			<td><input bind:value={user.situacion_economica.deudas_actuales[index].saldo_actual} placeholder="$m.n." required={index === 0 ? true : false}></td>
+			<td><input bind:value={user.situacion_economica.deudas_actuales[index].pago_mensual} placeholder="$m.n." required={index === 0 ? true : false}></td>
+		</tr>
+	{/each}
 </table>
