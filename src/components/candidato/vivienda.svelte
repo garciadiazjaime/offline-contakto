abefce2e20f003b7e0f9f21f7112f08bd38a8f02
jgarciadiaz <garciadiazjaime@gmail.com>
2021-04-08 13:38:14 -0500

    feat: [https://trello.com/c/tOFhdeH0]

--- src/components/candidato/vivienda.svelte
+++ src/components/candidato/vivienda.svelte
@@ -1,6 +1,6 @@
 <script>
 	export let user
-	const items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+	const items = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 
 	function tipoViviendaChange(event) {
 		user.situacion_vivienda.caracteristicas_vivienda.propia = event.target.value
