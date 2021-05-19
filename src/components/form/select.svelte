0debaec57ae1cd8fd24f66e11caa540d30d4a07e
jaime <garciadiazjaime@gmail.com>
2021-05-19 01:11:50 -0500

    feat: [https://trello.com/c/DYmy28LF]

---
+++ src/components/form/select.svelte
@@ -0,0 +1,20 @@
+<script>
+  export let handler
+  export let name
+  export let required
+</script>
+
+<style>
+	select {
+		height: 38px;
+    font-size: 26px;
+		width: 100%;
+	}
+</style>
+
+<!-- svelte-ignore a11y-no-onchange -->
+<select on:change={handler} name={name} required={required}>
+  <option value="">Seleccionar</option>
+  <option value="SI">Sí</option>
+  <option value="NO">No</option>
+</select>	
