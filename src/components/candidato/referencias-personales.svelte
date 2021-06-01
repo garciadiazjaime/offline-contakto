14df6ccb269cc49c05482077356e7a3de4292ce3
jaime <garciadiazjaime@gmail.com>
2021-06-01 13:53:41 -0500

    feat: [https://trello.com/c/DYmy28LF] tel

--- src/components/candidato/referencias-personales.svelte
+++ src/components/candidato/referencias-personales.svelte
@@ -1,15 +1,33 @@
 <script>
 	export let user
+
+	function telefonoHandler(event, index) {
+		const x = event.target.value.slice(0, 12).replace(/\D/g, '').match(/(\d{0,3})(\d{0,3})(\d{0,4})/);
+		const phone = !x[2] ? x[1] : x[1] + '-' + x[2] + (x[3] ? '-' + x[3] : '');
+
+		user.referencias[index].telefono = phone
+	}
+
+	function telSelectHandler(event, index) {
+		const value = event.target.value === "1" ? 'No tiene' : ''
+
+		user.referencias[index].telefono = value
+	}
 </script>
 
 <style>
 	span {
 		display: block;
 	}
-	input {
+
+	input, select {
 		height: 32px;
 		font-size: 26px;
 		width: 100%;
+	}
+
+	select {
+		height: 38px;
 	}
 
 	p {
@@ -17,13 +35,17 @@
 		align-items: center;
 	}
 
-	p span {
+	p span{
 		width: 240px;
 		padding-right: 6px;
 	}
 
 	h2 {
 		padding-top: 29px;
+	}
+
+	.paragraph span {
+		width: 475px;
 	}
 </style>
 
@@ -39,9 +61,14 @@
 		<span>Domicilio</span>
 		<input bind:value={user.referencias[index].domicilio} required>
 	</p>
-	<p>
+	<p class="paragraph">
 		<span>Teléfono</span>
-		<input bind:value={user.referencias[index].telefono} required>
+		<input on:keyup={event => telefonoHandler(event, index)} bind:value={user.referencias[index].telefono} placeholder="###-###-####" required>
+		<!-- svelte-ignore a11y-no-onchange -->
+		<select on:change={event => telSelectHandler(event, index)}>
+			<option value=0>Seleccionar</option>
+			<option value=1>No tiene</option>
+		</select>
 	</p>
 	<p>
 		<span>Tiempo de conocerlo</span>
