477345362ea9231aea56be5414264032ea2f821d
jaime <garciadiazjaime@gmail.com>
2021-07-05 16:21:31 -0500

    feat: [https://trello.com/c/16zZVpst] show image

--- src/components/candidato/adjuntos.svelte
+++ src/components/candidato/adjuntos.svelte
@@ -126,41 +126,41 @@
 <p>
 	<span>a. Interior derecho</span>
 	<button on:click={changeHandler} name="adj3">Seleccionar Archivo</button>
-	<img src={preview.adj3} alt="" />
 	{#if preview.adj3}
-	<strong on:click={clickHandlerDelete} data-file="adj3">x</strong>
+		<img src={preview.adj3.imageBuffer} alt="" />
+		<strong on:click={clickHandlerDelete} data-file="adj3">x</strong>
 	{/if}
 </p>
 <p>
 	<span>b. Interior izquierdo</span>
 	<button on:click={changeHandler} name="adj4">Seleccionar Archivo</button>
-	<img src={preview.adj4} alt="" />
 	{#if preview.adj4}
-	<strong on:click={clickHandlerDelete} data-file="adj4">x</strong>
+		<img src={preview.adj4.imageBuffer} alt="" />
+		<strong on:click={clickHandlerDelete} data-file="adj4">x</strong>
 	{/if}
 </p>
 <p>
 	<span>c. Exterior derecho</span>
 	<button on:click={changeHandler} name="adj5">Seleccionar Archivo</button>
-	<img src={preview.adj5} alt="" />
 	{#if preview.adj5}
-	<strong on:click={clickHandlerDelete} data-file="adj5">x</strong>
+		<img src={preview.adj5.imageBuffer} alt="" />
+		<strong on:click={clickHandlerDelete} data-file="adj5">x</strong>
 	{/if}
 </p>
 <p>
 	<span>d. Exterior izquierdo</span>
 	<button on:click={changeHandler} name="adj6">Seleccionar Archivo</button>
-	<img src={preview.adj6} alt="" />
 	{#if preview.adj6}
-	<strong on:click={clickHandlerDelete} data-file="adj6">x</strong>
+		<img src={preview.adj6.imageBuffer} alt="" />
+		<strong on:click={clickHandlerDelete} data-file="adj6">x</strong>
 	{/if}
 </p>
 <p>
 	<span>e. Frente</span>
 	<button on:click={changeHandler} name="adj9">Seleccionar Archivo</button>
-	<img src={preview.adj9} alt="" />
 	{#if preview.adj9}
-	<strong on:click={clickHandlerDelete} data-file="adj9">x</strong>
+		<img src={preview.adj9.imageBuffer} alt="" />
+		<strong on:click={clickHandlerDelete} data-file="adj9">x</strong>
 	{/if}
 </p>
 
@@ -169,33 +169,33 @@
 <p>
 	<span><b>3. Gestor Entrevistador</b></span>
 	<button on:click={changeHandler} name="adj10">Seleccionar Archivo</button>
-	<img src={preview.adj10} alt="" />
 	{#if preview.adj10}
-	<strong on:click={clickHandlerDelete} data-file="adj10">x</strong>
+		<img src={preview.adj10.imageBuffer} alt="" />
+		<strong on:click={clickHandlerDelete} data-file="adj10">x</strong>
 	{/if}
 </p>
 <p>
 	<span><b>4. Croquis</b></span>
 	<button on:click={changeHandler} name="adj13">Seleccionar Archivo</button>
-	<img src={preview.adj13} alt="" />
 	{#if preview.adj13}
-	<strong on:click={clickHandlerDelete} data-file="adj13">x</strong>
+		<img src={preview.adj13.imageBuffer} alt="" />
+		<strong on:click={clickHandlerDelete} data-file="adj13">x</strong>
 	{/if}
 </p>
 <p>
 	<span><b>5. Aviso Privacidad</b></span>
 	<button on:click={changeHandler} name="adj11">Seleccionar Archivo</button>
-	<img src={preview.adj11} alt="" />
 	{#if preview.adj11}
-	<strong on:click={clickHandlerDelete} data-file="adj11">x</strong>
+		<img src={preview.adj11.imageBuffer} alt="" />
+		<strong on:click={clickHandlerDelete} data-file="adj11">x</strong>
 	{/if}
 </p>
 <p>
 	<span><b>6. Constancia</b></span>
 	<button on:click={changeHandler} name="adj12">Seleccionar Archivo</button>
-	<img src={preview.adj12} alt="" />
 	{#if preview.adj12}
-	<strong on:click={clickHandlerDelete} data-file="adj12">x</strong>
+		<img src={preview.adj12.imageBuffer} alt="" />
+		<strong on:click={clickHandlerDelete} data-file="adj12">x</strong>
 	{/if}
 </p>
 
@@ -203,33 +203,33 @@
 <p>
 	<span>7.a Identificación con fotografia</span>
 	<button on:click={changeHandler} name="adj14">Seleccionar Archivo</button>
-	<img src={preview.adj14} alt="" />
 	{#if preview.adj14}
-	<strong on:click={clickHandlerDelete} data-file="adj14">x</strong>
+		<img src={preview.adj14.imageBuffer} alt="" />
+		<strong on:click={clickHandlerDelete} data-file="adj14">x</strong>
 	{/if}
 </p>
 <p>
 	<span>7.b Identificación con fotografia</span>
 	<button on:click={changeHandler} name="adj22">Seleccionar Archivo</button>
-	<img src={preview.adj22} alt="" />
 	{#if preview.adj22}
-	<strong on:click={clickHandlerDelete} data-file="adj22">x</strong>
+		<img src={preview.adj22.imageBuffer} alt="" />
+		<strong on:click={clickHandlerDelete} data-file="adj22">x</strong>
 	{/if}
 </p>
 <p>
 	<span>7.c Identificación con fotografia</span>
 	<button on:click={changeHandler} name="adj23">Seleccionar Archivo</button>
-	<img src={preview.adj23} alt="" />
 	{#if preview.adj23}
-	<strong on:click={clickHandlerDelete} data-file="adj23">x</strong>
+		<img src={preview.adj23.imageBuffer} alt="" />
+		<strong on:click={clickHandlerDelete} data-file="adj23">x</strong>
 	{/if}
 </p>
 <p>
 	<span>7.d Identificación con fotografia</span>
 	<button on:click={changeHandler} name="adj24">Seleccionar Archivo</button>
-	<img src={preview.adj24} alt="" />
 	{#if preview.adj24}
-	<strong on:click={clickHandlerDelete} data-file="adj24">x</strong>
+		<img src={preview.adj24.imageBuffer} alt="" />
+		<strong on:click={clickHandlerDelete} data-file="adj24">x</strong>
 	{/if}
 </p>
 
@@ -238,17 +238,17 @@
 <p>
 	<span><b>8. Acta de nacimiento</b></span>
 	<button on:click={changeHandler} name="adj17">Seleccionar Archivo</button>
-	<img src={preview.adj17} alt="" />
 	{#if preview.adj17}
-	<strong on:click={clickHandlerDelete} data-file="adj17">x</strong>
+		<img src={preview.adj17.imageBuffer} alt="" />
+		<strong on:click={clickHandlerDelete} data-file="adj17">x</strong>
 	{/if}
 </p>
 <p>
 	<span><b>9. Comprobante de domicilio</b></span>
 	<button on:click={changeHandler} name="adj16">Seleccionar Archivo</button>
-	<img src={preview.adj16} alt="" />
 	{#if preview.adj16}
-	<strong on:click={clickHandlerDelete} data-file="adj16">x</strong>
+		<img src={preview.adj16.imageBuffer} alt="" />
+		<strong on:click={clickHandlerDelete} data-file="adj16">x</strong>
 	{/if}
 </p>
 
@@ -258,17 +258,17 @@
 <p>
 	<span>a. Principal</span>
 	<button on:click={changeHandler} name="adj18">Seleccionar Archivo</button>
-	<img src={preview.adj18} alt="" />
 	{#if preview.adj18}
-	<strong on:click={clickHandlerDelete} data-file="adj18">x</strong>
+		<img src={preview.adj18.imageBuffer} alt="" />
+		<strong on:click={clickHandlerDelete} data-file="adj18">x</strong>
 	{/if}
 </p>
 <p>
 	<span>b. Extra</span>
 	<button on:click={changeHandler} name="adj37">Seleccionar Archivo</button>
-	<img src={preview.adj37} alt="" />
 	{#if preview.adj37}
-	<strong on:click={clickHandlerDelete} data-file="adj37">x</strong>
+		<img src={preview.adj37.imageBuffer} alt="" />
+		<strong on:click={clickHandlerDelete} data-file="adj37">x</strong>
 	{/if}
 </p>
 
@@ -277,81 +277,81 @@
 <p>
 	<span>Adicionales A</span>
 	<button on:click={changeHandler} name="adj19">Seleccionar Archivo</button>
-	<img src={preview.adj19} alt="" />
 	{#if preview.adj19}
-	<strong on:click={clickHandlerDelete} data-file="adj19">x</strong>
+		<img src={preview.adj19.imageBuffer} alt="" />
+		<strong on:click={clickHandlerDelete} data-file="adj19">x</strong>
 	{/if}
 </p>
 <p>
 	<span>Adicionales B</span>
 	<button on:click={changeHandler} name="adj20">Seleccionar Archivo</button>
-	<img src={preview.adj20} alt="" />
 	{#if preview.adj20}
-	<strong on:click={clickHandlerDelete} data-file="adj20">x</strong>
+		<img src={preview.adj20.imageBuffer} alt="" />
+		<strong on:click={clickHandlerDelete} data-file="adj20">x</strong>
 	{/if}
 </p>
 <p>
 	<span>Adicionales C</span>
 	<button on:click={changeHandler} name="adj21">Seleccionar Archivo</button>
-	<img src={preview.adj21} alt="" />
 	{#if preview.adj21}
-	<strong on:click={clickHandlerDelete} data-file="adj21">x</strong>
+		<img src={preview.adj21.imageBuffer} alt="" />
+		<strong on:click={clickHandlerDelete} data-file="adj21">x</strong>
 	{/if}
 </p>
 <p>
 	<span>Adicionales D</span>
 	<button on:click={changeHandler} name="adj29">Seleccionar Archivo</button>
-	<img src={preview.adj29} alt="" />
 	{#if preview.adj29}
-	<strong on:click={clickHandlerDelete} data-file="adj29">x</strong>
+		<img src={preview.adj29.imageBuffer} alt="" />
+		<strong on:click={clickHandlerDelete} data-file="adj29">x</strong>
 	{/if}
 </p>
 <p>
 	<span>Adicionales E</span>
 	<button on:click={changeHandler} name="adj30">Seleccionar Archivo</button>
-	<img src={preview.adj30} alt="" />
 	{#if preview.adj30}
-	<strong on:click={clickHandlerDelete} data-file="adj30">x</strong>
+		<img src={preview.adj30.imageBuffer} alt="" />
+		<strong on:click={clickHandlerDelete} data-file="adj30">x</strong>
 	{/if}
 </p>
 <p>
 	<span>Adicionales F</span>
 	<button on:click={changeHandler} name="adj31">Seleccionar Archivo</button>
-	<img src={preview.adj31} alt="" />
 	{#if preview.adj31}
-	<strong on:click={clickHandlerDelete} data-file="adj31">x</strong>
+		<img src={preview.adj31.imageBuffer} alt="" />
+		<strong on:click={clickHandlerDelete} data-file="adj31">x</strong>
 	{/if}
 </p>
 <p>
 	<span>Adicionales G</span>
 	<button on:click={changeHandler} name="adj32">Seleccionar Archivo</button>
-	<img src={preview.adj32} alt="" />
 	{#if preview.adj32}
-	<strong on:click={clickHandlerDelete} data-file="adj32">x</strong>
+		<img src={preview.adj32.imageBuffer} alt="" />
+		<strong on:click={clickHandlerDelete} data-file="adj32">x</strong>
 	{/if}
 </p>
 <p>
 	<span>Adicionales H</span>
 	<button on:click={changeHandler} name="adj33">Seleccionar Archivo</button>
-	<img src={preview.adj33} alt="" />
 	{#if preview.adj33}
-	<strong on:click={clickHandlerDelete} data-file="adj33">x</strong>
+		<img src={preview.adj33.imageBuffer} alt="" />
+		<strong on:click={clickHandlerDelete} data-file="adj33">x</strong>
 	{/if}
 </p>
 <p>
 	<span>Adicionales I</span>
 	<button on:click={changeHandler} name="adj34">Seleccionar Archivo</button>
-	<img src={preview.adj34} alt="" />
 	{#if preview.adj34}
-	<strong on:click={clickHandlerDelete} data-file="adj34">x</strong>
+		<img src={preview.adj34.imageBuffer} alt="" />
+		<strong on:click={clickHandlerDelete} data-file="adj34">x</strong>
 	{/if}
 </p>
 <p>
 	<span>Extra A</span>
 	<button on:click={changeHandler} name="adj35">Seleccionar Archivo</button>
-	<img src={preview.adj35} alt="" />
 	{#if preview.adj35}
-	<strong on:click={clickHandlerDelete} data-file="adj35">x</strong>
+		<img src={preview.adj35.imageBuffer} alt="" />
+		<strong on:click={clickHandlerDelete} data-file="adj35">x</strong>
 	{/if}
 </p>
 
